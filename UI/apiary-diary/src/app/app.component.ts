52ae1988202bbe43e75f53ef8272069e796cbd30
wojtekMate <wojtasiak.mateo@gmail.com>
2021-04-26 19:36:07 +0200

    docker-compose server, nginx default

--- UI/apiary-diary/src/app/app.component.ts
+++ UI/apiary-diary/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'apiary-diary';
-  private baseUrl = "https://localhost:5001/";
+  private baseUrl = "http://localhost:5000/";
   message : string = "";
   private headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
   private params = new HttpParams({fromString: 'name=term'});
