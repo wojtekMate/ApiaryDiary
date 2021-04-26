12821f60e4ca92a06ea79ef5690af2722fe5c9c7
wojtekMate <wojtasiak.mateo@gmail.com>
2021-04-26 12:48:08 +0200

    app builder and option pattern #10

---
+++ src/ApiaryDiary.Infrastructure/Extensions/Builders/ApiaryDiaryBuilder/IApiaryDiaryBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiaryDiary.Infrastructure.Extensions.Builders.ApiaryDiaryBuilder
+{
+    public interface IApiaryDiaryBuilder
+    {
+        IServiceCollection Services { get; }
+        IServiceProvider Build();
+    }
+}
