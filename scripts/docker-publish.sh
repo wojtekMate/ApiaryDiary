c276e456628c335b4cb6cf0510bd2f2bb2797cd8
wojtekMate <wojtasiak.mateo@gmail.com>
2021-05-17 17:52:04 +0200

    CI/CD fix

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,13 +1,8 @@
-DOCKER_ENV=''
-DOCKER_TAG=''
-case $TRAVIS_BRANCH in
-  master)
-    DOCKER_ENV=production
-    DOCKER_TAG=latest
-    ;;
-  develop)
-    DOCKER_ENV=development
-    DOCKER_TAG=dev
-    ;;    
-esac
-
+#!/bin/bash
+pass=$haslo
+Ip=46.101.251.246
+echo "$pass"
+echo root@"$Ip"
+sshpass -p "$pass"  ssh root@"$Ip" 'docker-compose up'
+echo wykonano
+echo $Ip
