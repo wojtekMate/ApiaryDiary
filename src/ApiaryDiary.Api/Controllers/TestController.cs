02ad69894817c5580a60f5bff867a851a5fb2a05
Mateusz Wojtasiak <42837771+wojtekMate@users.noreply.github.com>
2021-05-29 12:47:25 +0200

    Update TestController.cs

--- src/ApiaryDiary.Api/Controllers/TestController.cs
+++ src/ApiaryDiary.Api/Controllers/TestController.cs
@@ -29,7 +29,7 @@ namespace ApiaryDiary.Api.Controllers
         public string Get()
         {
             Thread.Sleep(2000);
-            return "Love you Gulia :* !!!!!";
+            return "cześć zibi";
         }
     }
 }
