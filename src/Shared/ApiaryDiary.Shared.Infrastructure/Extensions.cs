2b0f4f974cc0aa69a8d1feee3ed5e9ebc41e2909
wojtekmate <dev.mobile@orlen.pl>
2022-05-09 20:29:50 +0200

    Configure partManager, manage modules

--- src/Shared/ApiaryDiary.Shared.Infrastructure/Extensions.cs
+++ src/Shared/ApiaryDiary.Shared.Infrastructure/Extensions.cs
@@ -1,12 +1,14 @@
 ﻿using System.Reflection;
 using ApiaryDiary.Shared.Abstractions.Modules;
 using ApiaryDiary.Shared.Abstractions.Time;
+using ApiaryDiary.Shared.Infrastructure.Api;
 using ApiaryDiary.Shared.Infrastructure.Auth;
 using ApiaryDiary.Shared.Infrastructure.Contexts;
 using ApiaryDiary.Shared.Infrastructure.Email;
 using ApiaryDiary.Shared.Infrastructure.Time;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,6 +20,23 @@ public static class Extensions
 
     public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services,IList<Assembly> assemblies, IList<IModule> modules)
     {
+        var disabledModules = new List<string>();
+        using (var serviceProvider = services.BuildServiceProvider())
+        {
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            foreach (var (key, value) in configuration.AsEnumerable())
+            {
+                if (!key.Contains(":module:enabled"))
+                {
+                    continue;
+                }
+
+                if (!bool.Parse(value))
+                {
+                    disabledModules.Add(key.Split(":")[0]);
+                }
+            }
+        }
         services.AddCors(options =>
         {
             options.AddPolicy(CorsPolicy,
@@ -40,7 +59,24 @@ public static class Extensions
         services.AddAuth(modules);
         services.AddSingleton<IClock, UtcClock>();
         services.AddEmailService();
-        services.AddControllers();
+        services.AddControllers()
+            .ConfigureApplicationPartManager(manager =>
+            {
+                var removedParts = new List<ApplicationPart>();
+                foreach (var disabledModule in disabledModules)
+                {
+                    var parts = manager.ApplicationParts.Where(x => x.Name.Contains(disabledModule,
+                        StringComparison.InvariantCultureIgnoreCase));
+                    removedParts.AddRange(parts);
+                }
+
+                foreach (var part in removedParts)
+                {
+                    manager.ApplicationParts.Remove(part);
+                }
+                    
+                manager.FeatureProviders.Add(new InternalControllerFeatureProvider());
+            });
         return services;
     }
 
