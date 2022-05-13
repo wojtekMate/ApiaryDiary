4956dcda86074a4d5d21d3f2fcfbcc716880a987
wojtekmate <dev.mobile@orlen.pl>
2022-05-13 14:05:46 +0200

    Add connectiong string to travis build

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -2,5 +2,6 @@
 cd src/ApiaryDiary.Api
 sed -i -e "s/passwordMail/$passwordMail/g" appsettings.json
 sed -i -e "s/accountMail/$accountMail/g" appsettings.json
+sed -i -e "s/connectionStringValue/$connectionStringValue/g" appsettings.json
 dotnet restore
 dotnet build
