aa2162dbb6ffa7c736e8b2322c8e6bbd611877bb
wojtekmate <dev.mobile@orlen.pl>
2022-05-17 18:03:44 +0200

    Add logging

--- src/Shared/ApiaryDiary.Shared.Infrastructure/Services/AppInitializer.cs
+++ src/Shared/ApiaryDiary.Shared.Infrastructure/Services/AppInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApiaryDiary.Shared.Infrastructure.Email;
+using ApiaryDiary.Shared.Infrastructure.Postgres;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,11 +12,13 @@ namespace ApiaryDiary.Shared.Infrastructure.Services
     {
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger<AppInitializer> _logger;
+        private readonly IConfiguration _configuration;
 
-        public AppInitializer(IServiceProvider serviceProvider, ILogger<AppInitializer> logger)
+        public AppInitializer(IServiceProvider serviceProvider, ILogger<AppInitializer> logger, IConfiguration configuration)
         {
             _serviceProvider = serviceProvider;
             _logger = logger;
+            _configuration = configuration;
         }
         
         public async Task StartAsync(CancellationToken cancellationToken)
@@ -21,7 +26,10 @@ namespace ApiaryDiary.Shared.Infrastructure.Services
             var dbContextTypes = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(x => x.GetTypes())
                 .Where(x => typeof(DbContext).IsAssignableFrom(x) && !x.IsInterface && x != typeof(DbContext));
-
+            
+            var options =_configuration.GetOptions<PostgresOptions>("postgres");
+            var message = $"Baza danych:{options.ConnectionString}";
+            _logger.Log(LogLevel.Information,message);
             using var scope = _serviceProvider.CreateScope();
             foreach (var dbContextType in dbContextTypes)
             {
