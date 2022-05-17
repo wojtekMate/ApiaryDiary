e583cde3b174c40ca319b94459838849cfc492c1
wojtekmate <dev.mobile@orlen.pl>
2022-05-17 18:28:45 +0200

    show settings

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,5 +3,6 @@ cd src/Bootstrapper/ApiaryDiary.Bootstrapper
 sed -i -e "s/passwordMail/$passwordMail/g" appsettings.json
 sed -i -e "s/accountMail/$accountMail/g" appsettings.json
 sed -i -e "s/connectionStringValue/$connectionStringValue/g" appsettings.json
+cat appsettings.json
 dotnet restore
 dotnet build
