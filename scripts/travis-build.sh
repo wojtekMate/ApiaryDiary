f2dcd567b532451343f60951210bd04c3790febf
wojtekmate <dev.mobile@orlen.pl>
2022-05-18 07:35:57 +0200

    Build connectiong string in TravisCI

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -2,6 +2,10 @@
 cd src/Bootstrapper/ApiaryDiary.Bootstrapper
 sed -i -e "s/passwordMail/$passwordMail/g" appsettings.json
 sed -i -e "s/accountMail/$accountMail/g" appsettings.json
+
+connectionStringValue=''
+connectionStringValue= "$HOST;$PORT;$DATABASE;$USERNAME_DB;$PASSWORD_DB"
+
 sed -i -e "s/connectionStringValue/$connectionStringValue/g" appsettings.json
 cat appsettings.json
 dotnet restore
