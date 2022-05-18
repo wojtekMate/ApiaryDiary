b934666a58ab968d8d9772c25330332269f68047
wojtekmate <dev.mobile@orlen.pl>
2022-05-18 08:09:33 +0200

    fix building connection string

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,10 +3,9 @@ cd src/Bootstrapper/ApiaryDiary.Bootstrapper
 sed -i -e "s/passwordMail/$passwordMail/g" appsettings.json
 sed -i -e "s/accountMail/$accountMail/g" appsettings.json
 
-connectionStringValue=''
-connectionStringValue= "$HOST;$PORT;$DATABASE;$USERNAME_DB;$PASSWORD_DB"
-
+connectionStringValue="$HOST;$PORT;$DATABASE;$USERNAME_DB;$PASSWORD_DB"
 sed -i -e "s/connectionStringValue/$connectionStringValue/g" appsettings.json
 cat appsettings.json
+
 dotnet restore
 dotnet build
